6262278bb1432f071fd52e2654912de3597a624c
PJ <knightcode@gmail.com>
2013-02-10 16:45:50 -0500

    More login layout improvements

--- www/templates/login_template.php
+++ www/templates/login_template.php
@@ -1,7 +1,7 @@
 <?php 
 $context['blocks']['main'] = <<<END
     <div>
-    <h2>Login</h2>
+    <h2 class=center>Login</h2>
     <form class="login-form" name="form1" method="post" action="login_go.php">
         <div class=login-form-item><label>Name:</label><input name="UserName" type="text" size="20" id="UserName"></input></div>
         <div class=login-form-item><label>Password:</label><input name="Password" type="password" size="20"
@@ -9,11 +9,11 @@ $context['blocks']['main'] = <<<END
         <div class=login-form-item><input name="KeepInfo" type="checkbox" value="KeepInfo"></input>
         Save Information (7 days)</div>
         <div class=login-form-item><input name="Submit" type="submit" value="Log in"></input></div>
+        <div class=login-form-item>
+            <a href="forgot.php">Forgot password?</a>
+            <a href="SignUp.php">New Account</a>
+        </div>
     </form>
-    <div class=login-form-item>
-        <a href="forgot.php">Forgot password?</a>
-        <a href="SignUp.php">New Account</a>
-    </div>
     </div>
 END;
 
